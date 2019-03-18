c22601e0b23147ea9bb2b512825afa2643512669
romain <rmathieu13@gmail.com>
2019-03-18 15:46:29 +0100

    java drawer 50%

--- app/src/main/java/com/romain/mathieu/go4lunch/MainActivity.java
+++ app/src/main/java/com/romain/mathieu/go4lunch/MainActivity.java
@@ -1,14 +1,28 @@
 package com.romain.mathieu.go4lunch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.stetho.Stetho;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-
+//                    TopStoriesPageFragment.newInstance();
                     return true;
                 case R.id.navigation_dashboard:
 
@@ -35,8 +49,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+        Stetho.initializeWithDefaults(this);
+
+        setSupportActionBar(toolbar);
+        setTitle("My News");
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    //-------------
+    // NAVIGATION DRAWER
+    //-------------
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        // 6 - Show fragment after user clicked on a menu item
+        switch (id) {
+            case R.id.menu_drawer_1:
+                pager.setCurrentItem(0);
+                break;
+            case R.id.menu_drawer_2:
+                Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(myIntent);
+                break;
+            case R.id.menu_drawer_3:
+                pager.setCurrentItem(3);
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
