eac3e23be1f09b15d73bf52d288e115d794a6327
romain <rmathieu13@gmail.com>
2019-03-27 15:52:49 +0100

    create file myConstant

---
+++ app/src/main/java/com/romain/mathieu/go4lunch/utils/MyConstant.java
@@ -0,0 +1,7 @@
+package com.romain.mathieu.go4lunch.utils;
+
+public class MyConstant {
+
+    public static String BASE_URL = "https://maps.googleapis.com/";
+    public static String API_KEY = "AIzaSyBW10_Ie5wh-vwbEXEfWzk2zOFOQ_xfDWk";
+}
