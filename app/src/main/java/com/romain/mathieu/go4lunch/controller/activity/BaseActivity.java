1414c6245a5907d94bf4340c2863e680a0869af5
romain <rmathieu13@gmail.com>
2019-04-02 19:09:25 +0200

    clear

--- app/src/main/java/com/romain/mathieu/go4lunch/controller/activity/BaseActivity.java
+++ app/src/main/java/com/romain/mathieu/go4lunch/controller/activity/BaseActivity.java
@@ -6,12 +6,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.ButterKnife;
 
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+
 
     // --------------------
     // LIFE CYCLE
@@ -31,7 +32,11 @@ public abstract class BaseActivity extends AppCompatActivity {
     // --------------------
 
     @Nullable
-    protected FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
+    protected FirebaseUser getCurrentUser() {
+        return FirebaseAuth.getInstance().getCurrentUser();
+    }
 
-    protected Boolean isCurrentUserLogged(){ return (this.getCurrentUser() != null); }
+    protected Boolean isCurrentUserLogged() {
+        return (this.getCurrentUser() != null);
+    }
 }
