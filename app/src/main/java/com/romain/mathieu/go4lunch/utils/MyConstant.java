e3f14378a63fbc48c90b85a7566b4112e4479f3b
romain <rmathieu13@gmail.com>
2019-04-19 09:56:57 +0200

    change base url

--- app/src/main/java/com/romain/mathieu/go4lunch/utils/MyConstant.java
+++ app/src/main/java/com/romain/mathieu/go4lunch/utils/MyConstant.java
@@ -2,6 +2,6 @@ package com.romain.mathieu.go4lunch.utils;
 
 public class MyConstant {
 
-    public static String BASE_URL = "https://maps.googleapis.com/";
+    public static String BASE_URL = "https://maps.googleapis.com/maps/api/place/";
     public static String API_KEY = "AIzaSyBW10_Ie5wh-vwbEXEfWzk2zOFOQ_xfDWk";
 }
