2fab6aa822014312eda956608291c3b4862f1228
romain <rmathieu13@gmail.com>
2019-04-21 19:05:38 +0200

    update pojo

--- app/src/main/java/com/romain/mathieu/go4lunch/model/api/placeDetails/Result.java
+++ app/src/main/java/com/romain/mathieu/go4lunch/model/api/placeDetails/Result.java
@@ -4,17 +4,37 @@ package com.romain.mathieu.go4lunch.model.api.placeDetails;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class Result {
 
+    @SerializedName("formatted_address")
+    @Expose
+    private String formattedAddress;
+    @SerializedName("name")
+    @Expose
+    private String name;
     @SerializedName("formatted_phone_number")
     @Expose
     private String formattedPhoneNumber;
     @SerializedName("opening_hours")
     @Expose
     private OpeningHours openingHours;
+    @SerializedName("photos")
+    @Expose
+    private List<Photo> photos = null;
     @SerializedName("website")
     @Expose
     private String website;
+
+
+    public String getFormattedAddress() {
+        return formattedAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public String getFormattedPhoneNumber() {
         return formattedPhoneNumber;
@@ -22,6 +42,10 @@ public class Result {
 
     public OpeningHours getOpeningHours() {
         return openingHours;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
     }
 
     public String getWebsite() {
